7112d1fb24353de19cf459fb29a6377c92eee8c1
Vineet Kumar <vk.vempire@gmail.com>
2021-10-23 16:16:44 +0530

    bugs fixed dependencies upgraded

--- spring/src/App.js
+++ spring/src/App.js
@@ -1,18 +1,19 @@
-import "./App.css";
-import { Canvas } from "@react-three/fiber";
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+
 
 function App() {
   return (
-  <>
-    <div className="App">
-        <h1>vineet bsdka</h1>
-      </div>
+    <Canvas shadowMap sRGB>
+      <Sky sunPosition={[100, 20, 100]} />
+      <ambientLight intensity={0.25} />
+      <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
+      <Physics gravity={[0, -30, 0]}>
 
-      <Canvas>
-        
-      </Canvas>
-  </>
-    
+      </Physics>
+    </Canvas>
   );
 }
 
