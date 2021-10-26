35b14efc9469bab6e588faffa81a17ef1c96e8f5
Vineet Kumar <vk.vempire@gmail.com>
2021-10-26 20:29:47 +0530

    code cleaned

--- spring/src/App.js
+++ spring/src/App.js
@@ -1,10 +1,3 @@
-// import React from "react";
-// import { Canvas } from '@react-three/fiber';
-// import { OrbitControls, Stars } from '@react-three/drei';
-// import { Physics, usePlane, useBox } from '@react-three/cannon';
-// import "./App.css";
-
-
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
@@ -20,14 +13,6 @@ function App() {
     <div style={{height:"100%",width:"100%",position:"absolute"}}>
     <Canvas shadowMap sRGB>
       <Sky sunPosition={[100, 20, 100]} />
-      {/* <Stars
-          radius={100} // Radius of the inner sphere (default=100)
-          depth={50} // Depth of area where stars should fit (default=50)
-          count={5000} // Amount of stars (default=5000)
-          factor={4} // Size factor (default=4)
-          saturation={0} // Saturation 0-1 (default=0)
-          fade // Faded dots (default=false)
-      /> */}
       <ambientLight intensity={0.25} />
       <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
       <Hud position={[0, 0, -2]} />
@@ -42,47 +27,3 @@ function App() {
 }
 
 export default App;
-// function Box() {
-//   const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
-//   return (
-//     <mesh
-//       onClick={() => {
-//         api.velocity.set(0, 2, 0);
-//       }}
-//       ref={ref}
-//       position={[0, 2, 0]}
-//     >
-//       <boxBufferGeometry attach="geometry" />
-//       <meshLambertMaterial attach="material" color="hotpink" />
-//     </mesh>
-//   );
-// }
-
-// function Plane() {
-//   const [ref] = usePlane(() => ({
-//     rotation: [-Math.PI / 2, 0, 0],
-//   }));
-//   return (
-//     <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-//       <planeBufferGeometry attach="geometry" args={[100, 100]} />
-//       <meshLambertMaterial attach="material" color="lightblue" />
-//     </mesh>
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     <div style={{height:"100%",width:"100%",position:"absolute"}}>
-//     <Canvas>
-//       <OrbitControls />
-//       <Stars />
-//       <ambientLight intensity={0.1} />
-//       <spotLight position={[10, 15, 10]} angle={0.3} />
-//       <Physics>
-//         <Box />
-//         <Plane />
-//       </Physics>
-//     </Canvas>
-//     </div>
-//   );
-// }
