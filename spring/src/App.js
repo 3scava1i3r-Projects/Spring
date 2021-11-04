9f0befa56868fe6b7560729858deb6cf717ef9df
Vineet Kumar <vk.vempire@gmail.com>
2021-11-05 01:39:38 +0530

    fixes

--- spring/src/App.js
+++ spring/src/App.js
@@ -3,41 +3,10 @@ import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 
-
 import { Ground } from './components/Ground';
 import Cubes from './components/Cubes';
 import { Player } from './components/Player';
 import { Hud } from './components/Hud';
-
-function Box() {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
-  return (
-
-    <mesh
-      onClick={() => {
-        api.velocity.set(0, 2, 0);
-      }}
-      ref={ref}
-      position={[0, 2, 0]}
-    >
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
-    </mesh>
-  );
-}
-
-function Plane() {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-  }));
-  return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
-    </mesh>
-  );
-}
-
 
 function App() {
   return (
